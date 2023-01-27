9a7ebea236887b736f2591e04e14e7aa272ff559
root <root@graduate-work.ru-central1.internal>
2023-01-27 09:41:20 +0000

    feat: spark with clickhouse

--- ugc/backend/clickhouse_initer/mapping/node[3].sql
+++ ugc/backend/clickhouse_initer/mapping/node[3].sql
@@ -4,10 +4,10 @@ CREATE DATABASE IF NOT EXISTS movies_replica;
 CREATE TABLE IF NOT EXISTS movies.movie_frame_queue
 (
     movie_id                  String,
+    movie_duration            Int64,
     frame_time                Int64,
     `event.type`              String,
-    `event.timestamp`         Int64,
-    `event.generated_after`   Int64
+    `event.timestamp`         Int64
 )
 ENGINE=Kafka()
 SETTINGS
@@ -23,20 +23,20 @@ CREATE TABLE IF NOT EXISTS movies.movie_frame
     id                      UUID,
     user_id                 String,
     movie_id                String,
+    movie_duration          Int64,
     frame_time              Int64,
     event_type              String,
     event_timestamp         Int64,
-    event_generated_after   Int64,
     created_at              DateTime  DEFAULT now()
 )
-Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/movie_frame', 'replica3')
+Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/movie_frame', 'replica3')
 PARTITION BY toYYYYMMDD(created_at)
 ORDER BY (id);
 
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS movies.movie_frame_consumer
 TO movies.movie_frame
-AS SELECT movie_id, frame_time, _key as user_id, generateUUIDv4() as id, event.type as event_type, event.timestamp as event_timestamp, event.generated_after as event_generated_after
+AS SELECT movie_id, movie_duration, frame_time, _key as user_id, generateUUIDv4() as id, event.type as event_type, event.timestamp as event_timestamp
 FROM movies.movie_frame_queue;
 
 
@@ -45,10 +45,10 @@ CREATE TABLE IF NOT EXISTS default.movie_frame
     id                      UUID,
     user_id                 String,
     movie_id                String,
+    movie_duration          Int64,
     frame_time              Int64,
     event_type              String,
     event_timestamp         Int64,
-    event_generated_after   Int64,
     created_at              DateTime  DEFAULT now()
 )
 ENGINE = Distributed('main_cluster', '', movie_frame, rand());
--- ugc/backend/producer_storage/src/models/movie.py
+++ ugc/backend/producer_storage/src/models/movie.py
@@ -1,6 +1,7 @@
 import enum
-from pydantic import Field
+from pydantic import Field, validator
 from datetime import datetime
+from typing import Any
 
 from .base import JSONModel
 
@@ -14,13 +15,27 @@ class EventType(str, enum.Enum):
 class Event(JSONModel):
     type: EventType = Field(default=EventType.VIEWED)
     timestamp: int = Field(default_factory=lambda : int(datetime.utcnow().timestamp()))
-    generated_after: int | None = Field(default=None)
 
 
 class MovieFrameDatagram(JSONModel):
     movie_id: str
     frame_time: int
+    movie_duration: int
     event: Event
+
+    @validator('movie_duration', 'frame_time')
+    def frame_time_ge_zero(cls, value: int) -> int:
+        if value >= 0:
+            return value
+
+        raise ValueError('"frame_time" and "movie_duration" must be greater than zero')
+
+    @validator('movie_duration')
+    def movie_duration_ge_frame_time(cls, value: int, values: dict[str, Any], **kwargs) -> int:
+        if values.get('frame_time') is not None and value >= values.get('frame_time'):
+            return value
+
+        raise ValueError('"movie_duration" must be greater than "frame_time"')
 
 
 class MovieFrame(MovieFrameDatagram):
