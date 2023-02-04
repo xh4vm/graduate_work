4c47536b50c0c22e88c576d42a85cedb856afb5d
Kariyma <mariika-sun@yandex.ru>
2023-02-04 15:54:26 +0300

    Test clickhouse_pyspark_connector path

--- recommender_system/src/clickhouse_pyspark_connector.py
+++ recommender_system/src/clickhouse_pyspark_connector.py
@@ -3,17 +3,27 @@
 from pyspark.sql import SparkSession
 
 ABSOLUTE_PATH_TO_JARFILE = '/opt/jars/clickhouse-jdbc-0.3.2.jar'
-# CLICKHOUSE_JAR = f'file://{ABSOLUTE_PATH_TO_JARFILE}'
+CLICKHOUSE_JAR = f'file://{ABSOLUTE_PATH_TO_JARFILE}'
 
 
-def init_spark(app_name: str):
+def init_spark(app_name: str, num_executors: int, executor_memory='1G',
+               executor_cores=2, driver_memory='2G'):
 
     spark_session = (
         SparkSession
         .builder
         .appName(app_name)
         .master('spark://spark-master:7077')
-        .config('spark.jars', ABSOLUTE_PATH_TO_JARFILE) \
+        .config('spark.driver.memory', driver_memory)
+        .config('spark.executor.cores', executor_cores)
+        .config('spark.executor.memory', executor_memory)
+        .config('spark.executor.instances', num_executors)  # add more parameters if needed
+        .config('spark.driver.userClassPathFirst', 'true')
+        .config('spark.driver.extraLibraryPath',
+                f'/usr/hdp/2.6.5.0-292/hadoop/lib/native:{CLICKHOUSE_JAR}')  # example for multiple paths
+        .config('spark.executor.userClassPathFirst', 'true')
+        .config('spark.executor.extraLibraryPath',
+                f'/usr/hdp/2.6.5.0-292/hadoop/lib/native:{CLICKHOUSE_JAR}')
         )
 
     spark = (
@@ -25,7 +35,7 @@ def init_spark(app_name: str):
 
 
 def do_test():
-    spark = init_spark("Clickhouse test ")
+    spark = init_spark("Clickhouse test ", num_executors=3)
     query = "with t as (select user_id, movie_id, max(movie_duration) as movie_duration, sum(multiIf(event_type == 'starting', -1 * frame_time, event_type == 'stopped', frame_time, 0)) as metric, argMax(frame_time, created_at) as last_frame_time from (SELECT user_id, movie_id, frame_time, movie_duration, event_type, created_at from default.movie_frame ORDER BY created_at) GROUP BY user_id, movie_id) select user_id, movie_id, if(metric <= 0, last_frame_time + metric, metric) / movie_duration as metric from t"
 
     # database = os.environ.get('CH_DATABASE')
