aeeaf600ae75ba34c27fb4db953017ef3d7e4287
Kariyma <mariika-sun@yandex.ru>
2023-02-08 09:16:38 +0300

    Fix als_top module.

--- recommender_system/als_top/src/core/logger.py
+++ recommender_system/als_top/src/core/logger.py
@@ -4,4 +4,4 @@ logging.basicConfig(
     level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
 )
 
-logger = logging.getLogger('MONGODB-INITER')
+logger = logging.getLogger('ALS&TOP_MODULE')
